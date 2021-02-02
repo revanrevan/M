5129c8d12062149a0f434dee92b4f25a61874beb
revanrevan <revainblue21@gmail.com>
2021-02-02 08:18:08 +0700

    sementara 3

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -16,7 +16,7 @@
 		<div class="row">
 			<div class="col-12">
 				<nav class="main-nav">
-					<a href=" " class="logo">
+					<a href="/" class="logo">
 						<img src="/assets/images/logo.png" class="logoo" alt="Marikang"/>
 					</a>
 					<ul class="nav" style='display : {show ? "block" : "none"}'>
@@ -25,7 +25,7 @@
 						<li><a aria-current="{segment === 'caritukang' ? 'page' : undefined}" href="caritukang">Cari Tukang</a></li>
 						<li><a aria-current="{segment === 'artikel' ? 'page' : undefined}" href="artikel">Artikel</a></li>
 					</ul>
-					<a href=' ' class='menu-trigger {show ? "active" : ""}' on:click={toggle}>
+					<a href={segment} class='menu-trigger {show ? "active" : ""}' on:click={toggle}>
 						<span>Menu</span>
 					</a>
 				</nav>
--- src/routes/caritukang/detailtukang3.svelte
+++ src/routes/caritukang/detailtukang3.svelte
@@ -2,18 +2,18 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-				<img src="assets/images/Tukang1.jpeg" class="rounded img-fluid d-block mx-auto" alt="App">
+				<img src="assets/images/IMG-20201024-WA0023.jpg" class="rounded img-fluid d-block mx-auto" alt="App">
 			</div>
 			<div class="col-lg-1"></div>
 			<div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
 				<div class="left-heading">
-					<h2 class="section-title">Abd. Hakim</h2>
+					<h2 class="section-title">Eko Wahyudi</h2>
 				</div>
 				<div class="left-text">
 					<div class="row">
                         <div class="col-md-10 icon-box" data-aos="fade-up">
                             <h4>Umur</h4>
-                            <p>63 Tahun</p>
+                            <p>25 Tahun</p>
                         </div>
                         <div class="col-md-10 icon-box" data-aos="fade-up" data-aos-delay="100">
                             <h4>Alamat</h4>
@@ -21,7 +21,7 @@
                         </div>
                         <div class="col-md-10 icon-box" data-aos="fade-up" data-aos-delay="200">
                             <h4>Spesialis</h4>
-                            <p>Cat Dinding, Pasang Atap, Pasang Kramik</p>
+                            <p>Cat dan Las</p>
 						</div>
 						<div class="col-md-10 icon-box" data-aos="fade-up" data-aos-delay="200">
                             <h4>Status Kerja</h4>
@@ -29,7 +29,7 @@
 						</div>
 						<div class="col-md-10 icon-box" data-aos="fade-up" data-aos-delay="200">
 							<button class="main-button">
-								<a style="color: white;" href="https://wa.me/6282231274448?text=Saya%20ingin%20memesan%20tukang%2C%20atas%20nama%20Abd.%20Hakim">Chat via WhatsApp</a>
+								<a style="color: white;" href="https://wa.me/6282231274448?text=Saya%20ingin%20memesan%20tukang%2C%20atas%20nama%20Eko%20Wahyudi">Chat via WhatsApp</a>
 							</button>
                         </div>
                     </div>
